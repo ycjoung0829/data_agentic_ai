24d85fb71a81db4a9bdffd6ac9e7962d0655770c
ycjoung0829 <yoochanj@andrew.cmu.edu>
2025-03-10 13:21:52 -0400

    The result looks good for openai vision api.

--- backend/data_analysis_agent.py
+++ backend/data_analysis_agent.py
@@ -2,7 +2,9 @@ from image_analysis import analyze_image
 import pandas as pd 
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
+from openai import OpenAI
 import os 
+import base64
 
 
 
@@ -11,6 +13,8 @@ class DataAnalysisAgent:
         key = os.getenv('AZURE_SUBSCRIPTION_KEY')
         endpoint = os.getenv('AZURE_ENDPOINT') 
         self.computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(key))
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
+        self.openai_client = OpenAI(api_key=self.openai_api_key)
         # create a pandas dataframe
         self.dataset = dict() # create a dictionary to store data
         self.dataset['image_name'] = []
@@ -34,6 +38,36 @@ class DataAnalysisAgent:
             return description_result.captions[0].text #, [tag.name for tag in tags_result.tags]
         return ""
     
+    def run_analysis_openai(self, file):
+        """
+        _summary_: This function runs the data analysis agent on the image and returns the description of the image.
+        _params_: image: str
+        _returns_: str
+        """
+        with open(file, "rb") as f:
+            base64_image = base64.b64encode(f.read()).decode("utf-8")
+            response = self.openai_client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": "Describe what is seen in this image in one sentence",
+                            },
+                            {
+                                "type": "image_url",
+                                "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                            },
+                        ],
+                    }
+                ],
+            )
+
+            return response.choices[0].message.content
+        return "" 
+    
     def run_agent(self, folder_path, folder, timestamp):
         # Do some data analysis
         for file in folder:
@@ -41,7 +75,7 @@ class DataAnalysisAgent:
             print("file_path in agent:", file_path)
             file_path.parent.mkdir(parents=True, exist_ok=True)
 
-            text = self.run_analysis(file_path)
+            text = self.run_analysis_openai(file_path)
             self.dataset['image_name'].append(file.filename)
             self.dataset['description'].append(text)
             self.dataset['url'].append(file_path)
--- backend/data_schema_agent.py
+++ backend/data_schema_agent.py
@@ -1,6 +1,6 @@
 import csv
 import os 
-import openai 
+from openai import OpenAI 
 import pandas as pd 
 import json 
 
@@ -8,6 +8,8 @@ import json
 class DataSchemaAgent:
     def __init__(self):
         self.dataset = pd.read_csv("uploaded_images/result.csv")
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
+        self.client = OpenAI(api_key=self.openai_api_key)
 
     def extract_categories(self):
         """
@@ -15,13 +17,14 @@ class DataSchemaAgent:
         _params_: descriptions: pandas dataframe
         _returns_: str
         """
-        system_prompt = """You are a data schema agent whose task is to generate the most relevant categories that best classify the dataset given a dataset with image descriptions.\
+        self.dataset = pd.read_csv("uploaded_images/result.csv")
+        system_prompt = """You are a data schema agent whose task is to generate the categories that best classify the dataset.\
             The output format should be a dictionary where the key is the category name and the value is a list of the most relevant image name for that category."""
-        prompt = "Extract the most relevant categories that best classify the dataset with image name and descriptions."
+        prompt = "Generate the most relevant categories given the dataset with image name and descriptions."
         description_string = self.dataset[["image_name", "description"]].to_string(index=False) 
         print("description_string:", description_string)
-        prompt += "The following is the dataset with image name and descr" + description_string
-        response = openai.chat.completions.create(
+        prompt += "The following is the dataset with image name and description\n" + description_string
+        response = self.client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
                 {"role": "system", "content": system_prompt},
@@ -29,5 +32,5 @@ class DataSchemaAgent:
             ],
             temperature = 0.2
         )
-        print(response.choices[0].message.parsed)
-        return json.loads(response.choices[0].message.parsed)
+        print("content:",response.choices[0].message.content[len("   python"):-len("```")].strip())
+        return json.loads(response.choices[0].message.content[len("   python"):-len("```")].strip())
